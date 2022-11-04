d70b0c905473ae621739ba3e93e8e65712b0ef32
dominic754 <69826136+dominic754@users.noreply.github.com>
2022-11-04 08:29:32 -0500

    Create main.cpp

---
+++ cpp/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    double firstnum;
+    double secondnum;
+    
+    std::cout << "Input your first number" << endl;
+    
+    
+    std::cin >> firstnum;
+    
+    std::cout << "Input your second number" << endl;
+    
+    
+    std::cin >> secondnum;
+    
+    std::cout << "\n";
+    
+    
+    
+    
+    std::cout << firstnum + secondnum;
+    
+    return 0;
+}
