83f2eac924ebf7605d25a4a9552e76177fd7cb41
dominic754 <69826136+dominic754@users.noreply.github.com>
2022-11-04 16:30:32 -0500

    Create main.ps1

---
+++ PowerShell/main.ps1
@@ -0,0 +1,6 @@
+Write-Host "Welcome to The PowerShell Calculator"
+[int]$firstNum= Read-Host -Prompt "Input your first number `n"
+[int]$secondNum= Read-Host -Prompt "Input your second number `n"
+$sum= $firstNum + $secondNum
+$sum
+
