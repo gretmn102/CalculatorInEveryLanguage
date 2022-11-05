e713e505d8c5a63b130ef555c285ea108624f984
dominic754 <69826136+dominic754@users.noreply.github.com>
2022-11-05 11:05:40 -0500

    Update main.ps1
    Added the feature to select which operation the user desired.

--- PowerShell/main.ps1
+++ PowerShell/main.ps1
@@ -1,6 +1,23 @@
 Write-Host "Welcome to The PowerShell Calculator"
 [int]$firstNum= Read-Host -Prompt "Input your first number `n"
 [int]$secondNum= Read-Host -Prompt "Input your second number `n"
-$sum= $firstNum + $secondNum
+$operator= Read-Host -Prompt "Input your desired Operator ( + | - | * | / )"
+if ( $operator -eq "+" )
+{
+    $sum= $firstNum + $secondNum
+}
+if ( $operator -eq "-" )
+{
+    $sum= $firstNum - $secondNum
+}
+if ( $operator -eq "*" )
+{
+    $sum= $firstNum + $secondNum
+}
+if ( $operator -eq "/" )
+{
+    $sum= $firstNum / $secondNum
+}
+
 $sum
 
