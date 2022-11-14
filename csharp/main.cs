ca4c63be04a50ddaf459d51c47648b3301143c79
dominic754 <69826136+dominic754@users.noreply.github.com>
2022-11-13 16:46:05 -0800

    Update main.cs

--- csharp/main.cs
+++ csharp/main.cs
@@ -1,16 +1,48 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Program {
-  public static void Main (string[] args) {
-    
-    Console.WriteLine ("Input your first number");
-    int firstNum = Convert.ToInt32(Console.ReadLine());
+namespace CSCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your first number");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
 
-    Console.WriteLine ("Input your second number");
-    int secondNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input your second number");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
 
-    Console.WriteLine(firstNum + secondNum);
+            Console.WriteLine("Input your desired operation [+, -, *, /]");
+            string op = Console.ReadLine();  
 
-    Console.ReadKey();
-  }
+            if (op == "+") { 
+                Console.WriteLine(firstNum + secondNum);
+                Console.ReadKey();
+            }
+
+            else if (op == "-") {
+                Console.WriteLine(firstNum - secondNum);
+                Console.ReadKey();
+            }
+
+            else if (op == "*")
+            {
+                Console.WriteLine(firstNum * secondNum);
+                Console.ReadKey();
+            }
+
+            else if (op == "/")
+            {
+                Console.WriteLine(firstNum / secondNum);
+                Console.ReadKey();
+            }
+
+
+            
+        }
+    }
 }
